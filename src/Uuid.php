d40431000a4104be213efc4b1629c16928d08c64
SandeepKr <code@sandeepkr.com>
2023-07-10 10:39:07 +0530

    init

--- src/Uuid.php
+++ src/Uuid.php
@@ -9,15 +9,113 @@ class Uuid{
 
     /*
     |
-    | 
+    | Class variables using for UUID generation
     |
     */
+    public int $NISHADIL_UUID_VERSION;
 
 
 
     
+
+
+
+
     function __construct() {
 
+    }
+
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 1
+    |
+    */
+    public function v1() {
+        
+    }
+
+
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 2
+    |
+    */
+    public function v2() {
+        
+    }
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 3
+    |
+    */
+    public function v3() {
+        
+    }
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 4
+    |
+    */
+    public function v4() {
+        
+    }
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 5
+    |
+    */
+    public function v5() {
+        
+    }
+
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 6
+    |
+    */
+    public function v6() {
+        
+    }
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 7
+    |
+    */
+    public function v7() {
+        
+    }
+
+
+
+
+    /*
+    |
+    | UUID generation using UUID standard version 8 
+    |
+    */
+    public function v8() {
+        
     }
 
 }
