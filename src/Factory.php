b65ebc8ca8f9a4e8405f4eea953497aec064662b
SandeepKr <code@sandeepkr.com>
2023-07-12 00:23:04 +0530

    UUID v4 generation done

--- src/Factory.php
+++ src/Factory.php
@@ -12,7 +12,6 @@ class Factory{
     }
 
 
-    // public static function generate( $NISHADIL_UUID_VERSION, $NISHADIL_UUID_NODE ): ?string {
     public static function generate( array $NISHADIL_UUID_PREPDATA ): ?string {
         // return $NISHADIL_UUID_VERSION."-".$NISHADIL_UUID_NODE;
         // return self::$UUIDv1->generate();
@@ -20,7 +19,7 @@ class Factory{
 
         switch($NISHADIL_UUID_VERSION){
           case 1:
-            return self::v1( $NISHADIL_UUID_NODE, $NISHADIL_UUID_CLOCKSEQ );
+            return self::v1( $NISHADIL_UUID_VERSION, $NISHADIL_UUID_NODE, $NISHADIL_UUID_CLOCKSEQ );
             break;
           case 2:
             return self::v2();
@@ -29,7 +28,7 @@ class Factory{
             return self::v3();
             break;
           case 4:
-            return self::v4();
+            return self::v4( $NISHADIL_UUID_VERSION, $NISHADIL_UUID_NODE, $NISHADIL_UUID_CLOCKSEQ );
             break;
           case 5:
             return self::v5();
